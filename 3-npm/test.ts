ca64cbd32be13aeef521c20e9465935d18b7ccac
xiao <1325533724@qq.com>
2022-08-16 22:42:40 +0800

    git ignored

--- 3-npm/test.ts
+++ 3-npm/test.ts
@@ -2,6 +2,14 @@ const moment = require('moment');
 const now = Date.now()
 const format = moment(now).format('YYYY-MM-DD HH:mm:ss')
 
-
-
+// TODO 
+// ! fdasfdas
+// ~ jflfjsklafas
+// ^ fdasfjdklas
+// & fjdsajfkldasjf
+// * xxxkkxxk
+// // fdjafljdaslf
 console.log('moment formart date: ', format)
+
+
+const fn = () => {}
